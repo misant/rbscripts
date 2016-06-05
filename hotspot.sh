0e644099eaba30f6300c3347c2ac46afbcfdffec
Evgeniy Bekhterev <jbe@mail.ru>
2016-06-06 02:14:56 +0300

    Uptime with 0m processing added
    List of mikrotiks processed for connection also

--- hotspot.sh
+++ hotspot.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+cat list.txt | while read -r line ; do
+
+ssh admin@$line ip hot host pr > hosts
+ssh admin@$line ip hot active pr > active
+ssh admin@$line ip hot print > hotspot
+ssh admin@$line inter pri deta > interfaces
+
 
 CurrentCalc=`date +%s`
 CurrentTime=`date +%Y-%m-%d:%H:%M:%S`
@@ -14,18 +21,40 @@ echo "Active connections" >> output
 cat active | grep h | while read -r line ; do
     DeviceIP=`echo $line | awk '{print($4)}'`
     DeviceUptime=`echo $line | awk '{print($5)}'`
-    UptimeH=`echo $DeviceUptime | awk -F'[h]' '{print $1}'`
-    if [ "$UptimeH" == "$DeviceUptime" ]; then
-        let UptimeH=0
-        UptimeM=`echo $DeviceUptime | awk -F'[m]' '{print $1}'`
-        UptimeS=`echo $DeviceUptime | awk -F'[s]' '{print $1}'| awk -F'[m]' '{print $2}'`
+    echo "DeviceUptime = $DeviceUptime"
+
+
+
+    if [[ $DeviceUptime == *"h"* ]];
+    then
+        UptimeH=`echo $DeviceUptime | cut -d 'h' -f 1`
     else
-        UptimeM=`echo $DeviceUptime | awk -F'[m]' '{print $1}'| awk -F'[h]' '{print $2}'`
-        UptimeS=`echo $DeviceUptime | awk -F'[s]' '{print $1}'| awk -F'[m]' '{print $2}'`
+        UptimeH=0
+        DeviceUptime=`echo "0h$DeviceUptime"`
+    fi
+
+    if [[ $DeviceUptime == *"s"* ]];
+    then
+        if [[ $DeviceUptime == *"m"* ]];
+        then
+            UptimeS=`echo $DeviceUptime | awk -F'[s]' '{print $1}'| awk -F'[m]' '{print $2}'`
+        else
+            UptimeS=`echo $DeviceUptime | awk -F'[s]' '{print $1}'| awk -F'[h]' '{print $2}'`
+        fi
+    else
+        UptimeS=0
+        DeviceUptime=`echo $DeviceUptime'0s'`
+    fi
+
+    if [[ $DeviceUptime == *"m"* ]];
+    then
+        UptimeM=`echo $DeviceUptime | grep -o -P '(?<=h).*(?=m)'`
+    else
+        UptimeM=0
     fi
     let UptimeCalc="$UptimeH * 3600 + $UptimeM * 60 + $UptimeS"
     let ConnCalc="$CurrentCalc - $UptimeCalc"
-    ConnTime=`date -u -d @${ConnCalc}`
+    ConnTime=`date -u -d @${ConnCalc} +%Y-%m-%d:%H:%M:%S`
     echo $ConnTime
     DeviceMAC=`cat hosts | grep "$DeviceIP " | awk '{print($3)}'`
     echo "Device IP = $DeviceIP"
@@ -39,5 +68,6 @@ cat hosts | grep 5m | while read -r line ; do
     ConnStatus=`echo $line | awk '{print($2)}'`
     echo "Device MAC = $DeviceMAC"
     echo "Device MAC = $ConnStatus"
-    echo "$HotspotIntMAC,$DeviceMAC,$ConnStatus,unautharised,$CurrentTime" >> output
+    echo "$HotspotIntMAC,$DeviceMAC,$ConnStatus,unautharized,$CurrentTime" >> output
+done
 done
