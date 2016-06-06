2f35381570ade446da4bd1af5822cb95f50cd8b4
Evgeniy Bekhterev <jbe@mail.ru>
2016-06-06 15:36:32 +0300

    Hotspot mac list added
    Also we check if its not too old

--- hotspot.sh
+++ hotspot.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
-cat list.txt | while read -r line ; do
-
-ssh admin@$line ip hot host pr > hosts
-ssh admin@$line ip hot active pr > active
-ssh admin@$line ip hot print > hotspot
-ssh admin@$line inter pri deta > interfaces
-
 
 CurrentCalc=`date +%s`
 CurrentTime=`date +%Y-%m-%d:%H:%M:%S`
 
-echo $CurrentCalc
-HotspotIntName=`cat hotspot | grep m | awk '{print($3)}'`
-echo "Hotspot interface name = $HotspotIntName"
-HotspotIntMAC=`cat interfaces | grep -A 1 $HotspotIntName | awk '{print($1)}' | cut -d= -f2`
-HotspotIntMAC=( $HotspotIntMAC )
-HotspotIntMAC=${HotspotIntMAC[1]}
-#echo "Hotspot interface mac = $HotspotIntMAC" >> output
+cat list.txt | while read -r line ; do
+[[ -f mac ]] || touch mac
+
+if grep -q $line "mac"; then
+    ssh admin@$line ip hot host pr > hosts
+    ssh admin@$line ip hot active pr > active
+    HotspotIntMAC=`grep $line "mac" | awk '{print($2)}'`
+    MACDate=`grep $line "mac" | awk '{print($3)}'`
+    MACSecs=`grep $line "mac" | awk '{print($4)}'`
+    if (( "$CurrentCalc - $MACSecs" > 86400 )); then
+        ssh admin@$line ip hot print > hotspot
+        ssh admin@$line inter pri deta > interfaces
+        HotspotIntName=`cat hotspot | grep m | awk '{print($3)}'`
+        echo "Too old!"
+        HotspotIntMAC=`cat interfaces | grep -A 1 $HotspotIntName | awk '{print($1)}' | cut -d= -f2`
+        HotspotIntMAC=( $HotspotIntMAC )
+        HotspotIntMAC=${HotspotIntMAC[1]}
+        sed -i -e "/$line/d" ./mac
+        echo "$line $HotspotIntMAC $CurrentTime $CurrentCalc" >> mac
+
+    else
+        echo "Mac is already known, $HotspotIntMAC was written $MACDate"
+    fi
+else
+    ssh admin@$line ip hot host pr > hosts
+    ssh admin@$line ip hot active pr > active
+    ssh admin@$line ip hot print > hotspot
+    ssh admin@$line inter pri deta > interfaces
+
+    HotspotIntName=`cat hotspot | grep m | awk '{print($3)}'`
+    echo "Hotspot interface name = $HotspotIntName"
+    HotspotIntMAC=`cat interfaces | grep -A 1 $HotspotIntName | awk '{print($1)}' | cut -d= -f2`
+    HotspotIntMAC=( $HotspotIntMAC )
+    HotspotIntMAC=${HotspotIntMAC[1]}
+    echo "$line $HotspotIntMAC $CurrentTime $CurrentCalc" >> mac
+    echo "Mac is uknown"
+
+fi
+
 echo "Active connections" >> output
 cat active | grep h | while read -r line ; do
     DeviceIP=`echo $line | awk '{print($4)}'`
@@ -71,3 +96,8 @@ cat hosts | grep 5m | while read -r line ; do
     echo "$HotspotIntMAC,$DeviceMAC,$ConnStatus,unautharized,$CurrentTime" >> output
 done
 done
+
+rm -f interfaces
+rm -f hotspot
+rm -f active
+rm -f hosts
